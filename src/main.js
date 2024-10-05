cefc48986570eedaa1b5dd9c59a5935e7ec4a335
Nerea Battauz <nereebattauz@gmail.com>
2024-10-05 00:52:36 -0300

    lograr traer datos segun nombre

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const hash = "21a300809c6bd1a8f0eeb61d1578a609";
 const parametrosAutenticacion = `?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
 
 const resultadosBusqueda = document.getElementById("resultados-busqueda");
-const cantidadResultados = document.getElementById("cantidad-resultados"); 
+const cantidadResultados = document.getElementById("cantidad-resultados");
 const inputBuscar = document.getElementById("input-buscar");
 const selectType = document.getElementById("select-type");
 const selectOrder = document.getElementById("select-order");
@@ -17,16 +17,17 @@ const btnAnterior = document.getElementById("btn-anterior");
 const btnPrimero = document.getElementById("btn-primero");
 const btnUltimo = document.getElementById("btn-ultimo");
 
-//INICIO
+//INICIO?
 //Obtener datos API
 
-let dataComics = "";
-let dataCharacters = "";
-let cantidadComics = 0;
-let cantidadCharacters = 0;
+let dataResultados = "";
+let totalResultados = 0;
+let urlSearch = new URL(
+  `${urlApi}${selectType.value}${parametrosAutenticacion}`
+);
 
 function traerAPIs() {
-  fetch(urlApi + selectType.value + parametrosAutenticacion, {
+  fetch(urlSearch, {
     method: "GET",
     headers: {
       //Authorization: `${publicKey}`,
@@ -35,131 +36,62 @@ function traerAPIs() {
   })
     .then((response) => response.json())
     .then((data) => {
+      resultadosBusqueda.innerHTML = "";
       totalResultados = data.data.total;
       dataResultados = data.data.results;
       mostrarCantResultados();
+      getCards();
     })
     .catch((error) => console.error(error));
 }
 traerAPIs();
 
-btnBuscar.addEventListener("click", () => {selectType.value="characters"; traerAPIs()})
-
 //Cantidad de resultados totales
 function mostrarCantResultados() {
   cantidadResultados.textContent = `${totalResultados} RESULTADOS`;
 }
-  
 
+// Generar cards characters
+function getComicCards() {
+  dataResultados.forEach((comic) => {
+    const urlThumbsComic =
+      `${comic.thumbnail.path}` + "." + `${comic.thumbnail.extension}`;
+    const comicTitle = comic.title;
 
-/* //Miniaturas y titulos comics inicio
+    const tarjetaComic = document.createElement("div");
+    tarjetaComic.classList.add("w-full", "sm:w-1/3", "lg:w-1/5", "mb-8");
+    tarjetaComic.id = "tarjeta-comic";
 
-function getThumbsAndTitles() {
-  if (selectType.value == "comics") {
-    dataComics.forEach((comic) => {
-      const urlThumbs =
-        `${comic.thumbnail.path}` + "." + `${comic.thumbnail.extension}`;
-      const comicTitle = comic.title;
+    const imgTarjetaComic = document.createElement("img");
+    imgTarjetaComic.classList.add(
+      "object-cover",
+      "items-center",
+      "justify-center",
+      "shadow-xl",
+      "w-11/12",
+      "h-96"
+    );
+    imgTarjetaComic.id = "img-tarjeta-comic";
+    imgTarjetaComic.src = `${urlThumbsComic}`;
 
-      //crear lista comics inicio
-      const tarjetaComic = document.createElement("div");
-      tarjetaComic.classList.add("w-full", "sm:w-1/3", "lg:w-1/5", "mb-8");
-      tarjetaComic.id = "tarjeta-comic";
+    const tituloTarjetaComic = document.createElement("h3");
+    tituloTarjetaComic.classList.add("font-bold", "text-md", "mt-1", "w-11/12");
+    tituloTarjetaComic.id = "titulo-tarjeta-comic";
+    tituloTarjetaComic.textContent = `${comicTitle}`;
 
-      const imgTarjetaComic = document.createElement("img");
-      imgTarjetaComic.classList.add(
-        "object-cover",
-        "items-center",
-        "justify-center",
-        "shadow-xl",
-        "w-11/12",
-        "h-96"
-      );
-      imgTarjetaComic.id = "img-tarjeta-comic";
-      imgTarjetaComic.src = `${urlThumbs}`;
-
-      const tituloTarjetaComic = document.createElement("h3");
-      tituloTarjetaComic.classList.add(
-        "font-bold",
-        "text-md",
-        "mt-1",
-        "w-11/12"
-      );
-      tituloTarjetaComic.id = "titulo-tarjeta-comic";
-      tituloTarjetaComic.textContent = `${comicTitle}`;
-
-      resultadosBusqueda.appendChild(tarjetaComic);
-      tarjetaComic.appendChild(imgTarjetaComic);
-      tarjetaComic.appendChild(tituloTarjetaComic);
-    });
-  } else if (selectType.value == "characters") {
-    resultadosBusqueda.innerHTML = "";
-    dataCharacters.forEach((character) => {
-      const urlThumbs =
-        `${character.thumbnail.path}` +
-        "." +
-        `${character.thumbnail.extension}`;
-      const characterName = character.name;
-
-      //crear lista comics inicio
-      const tarjetaCharacter = document.createElement("div");
-      tarjetaCharacter.classList.add("w-full", "sm:w-1/4", "lg:w-1/6", "mb-8");
-      tarjetaCharacter.id = "tarjeta-character";
-
-      const imgTarjetaCharacter = document.createElement("img");
-      imgTarjetaCharacter.classList.add(
-        "object-cover",
-        "items-center",
-        "justify-center",
-        "shadow-xl",
-        "w-11/12",
-        "h-96"
-      );
-      imgTarjetaCharacter.id = "img-tarjeta-character";
-      imgTarjetaCharacter.src = `${urlThumbs}`;
-
-      const tituloTarjetaCharacter = document.createElement("h3");
-      tituloTarjetaCharacter.classList.add(
-        "font-bold",
-        "text-md",
-        "mt-1",
-        "w-11/12"
-      );
-      tituloTarjetaCharacter.id = "titulo-tarjeta-character";
-      tituloTarjetaCharacter.textContent = `${characterName}`;
-
-      resultadosBusqueda.appendChild(tarjetaCharacter);
-      tarjetaCharacter.appendChild(imgTarjetaCharacter);
-      tarjetaCharacter.appendChild(characterName);
-    });
-  }
+    resultadosBusqueda.appendChild(tarjetaComic);
+    tarjetaComic.appendChild(imgTarjetaComic);
+    tarjetaComic.appendChild(tituloTarjetaComic);
+  });
 }
 
-fetch(urlApi + urlCharacters + parametrosAutenticacion, {
-  method: "GET",
-  headers: {
-    //Authorization: `${publicKey}`,
-    "Content-type": "application/json",
-  },
-})
-  .then((response) => response.json())
-  .then((data) => {
-    cantidadCharacters = data.data.total;
-    dataCharacters = data.data.results;
-  })
-  .catch((error) => console.error(error));
-
- */
-/* 
-//Traer personajes
-
-function getCharacters() {
-  dataCharacters.forEach((character) => {
-    const urlThumbs =
+// Generar cards characters
+function getCharacterCards() {
+  dataResultados.forEach((character) => {
+    const urlThumbsCharacter =
       `${character.thumbnail.path}` + "." + `${character.thumbnail.extension}`;
     const characterName = character.name;
 
-    //crear lista comics inicio
     const tarjetaCharacter = document.createElement("div");
     tarjetaCharacter.classList.add("w-full", "sm:w-1/4", "lg:w-1/6", "mb-8");
     tarjetaCharacter.id = "tarjeta-character";
@@ -171,36 +103,56 @@ function getCharacters() {
       "justify-center",
       "shadow-xl",
       "w-11/12",
-      "h-96"
+      "h-72"
     );
     imgTarjetaCharacter.id = "img-tarjeta-character";
-    imgTarjetaCharacter.src = `${urlThumbs}`;
+    imgTarjetaCharacter.src = `${urlThumbsCharacter}`;
 
-    const tituloTarjetaCharacter = document.createElement("h3");
-    tituloTarjetaCharacter.classList.add(
+    const nombreCharacter = document.createElement("h3");
+    nombreCharacter.classList.add(
       "font-bold",
       "text-md",
-      "mt-1",
-      "w-11/12"
+      "p-2",
+      "w-11/12",
+      "bg-black",
+      "h-20",
+      "text-white"
     );
-    tituloTarjetaCharacter.id = "titulo-tarjeta-character";
-    tituloTarjetaCharacter.textContent = `${characterName}`;
+    nombreCharacter.id = "titulo-tarjeta-comic";
+    nombreCharacter.textContent = `${characterName}`;
 
     resultadosBusqueda.appendChild(tarjetaCharacter);
     tarjetaCharacter.appendChild(imgTarjetaCharacter);
-    tarjetaCharacter.appendChild(characterName);
+    tarjetaCharacter.appendChild(nombreCharacter);
   });
 }
 
-
+//Obtener cards
+function getCards() {
+  if (selectType.value == "comics") {
+    getComicCards();
+  } else if (selectType.value == "characters") {
+    getCharacterCards();
+  }
+}
 
 // Actualizar resultados
 
-function actualizarResultados() {
-  resultadosBusqueda.innerHTML = "";
+function aplicarBusqueda() {
+  inputBuscar.addEventListener("input", (event) => {
+    inputBuscar.value = event.target.value;
+    const searchParams = urlSearch.searchParams;
+    searchParams.set("title", `${inputBuscar.value}`);
+    urlSearch.toString();
+    traerAPIs();
+  });
 }
 
-// Mostrar más resultados
+btnBuscar.addEventListener("click", aplicarBusqueda());
+/*   ; */
+//btnBuscar.addEventListener("click", aplicarBusqueda());
+
+/* // Mostrar más resultados
 
 btnSiguiente.addEventListener("click", () => {
   actualizarResultados();
@@ -208,14 +160,5 @@ btnSiguiente.addEventListener("click", () => {
   btnSiguiente.classList.add("bg-red-600");
   //getThumbsAndTitles(); Esto se aplica pero no se actualiza
 });
-
-//Realizar búsqueda
-
-const urlSearch = new URL(
-  `http://gateway.marvel.com/v1/public/${selectType.value}${parametrosAutenticacion}`
-);
-const params = urlSearch.searchParams;
-params.set("title", `${inputBuscar.value}`);
-
-btnBuscar.addEventListener("click", aplicarBusqueda);
  */
+//Realizar búsqueda
